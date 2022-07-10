4396cb2ac5c685921e4d68477dbf407a36485097
matheussilvatorres <matheustorres201900@gmail.com>
2022-07-10 18:27:14 -0300

    ex auka 04

---
+++ POO/aula04/aula04.java
@@ -0,0 +1,9 @@
+package aula04;
+
+public class aula04 {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta();
+        c1.setModelo("BIC");
+        c1.status();
+    }
+}
